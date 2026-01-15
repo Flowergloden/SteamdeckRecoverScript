41695375d821de001bd028e038c17d830a71fe00
huizeyang <xdhuizeyang@outlook.com>
2026-01-15 13:00:13 +0800

    feat: add hooks

---
+++ post_hook.sh
@@ -0,0 +1,3 @@
+# add your custom hooks here
+# this would be executed after importing pkglist
+echo -e "post hook execution starts"
---
+++ pre_hook.sh
@@ -0,0 +1,3 @@
+# add your custom hooks here
+# this would be executed before importing pkglist
+echo -e "pre hook execution starts"
